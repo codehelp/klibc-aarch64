1ea28e11dc23259f2830c11bf0772504c2598eea
Neil Williams <codehelp@debian.org>
2013-01-12 22:30:05 +0000

    implement the setjmp and longjmp with jmpbuf as per the ARMv8 ISA

--- usr/include/arch/aarch64/klibc/archsetjmp.h
+++ usr/include/arch/aarch64/klibc/archsetjmp.h
@@ -5,8 +5,13 @@
 #ifndef _KLIBC_ARCHSETJMP_H
 #define _KLIBC_ARCHSETJMP_H
 
+/* 0-7 are temporary, starting at x8, skip x16, go up to x30
+also store the d registers, d8 to d15.
+*/
+
 struct __jmp_buf {
-	unsigned int regs[10];
+	uint64_t __gregs[22];
+	uint64_t __fpregs[8];
 };
 
 typedef struct __jmp_buf jmp_buf[1];
